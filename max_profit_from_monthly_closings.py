9df83f647effc9ca2d8be2b6b7c30f0387e4473e
Akshesh <aksheshdoshi@gmail.com>
2022-01-29 07:45:17 +0530

    Support tsv output format in max_profit_from_monthly_closings.py

--- max_profit_from_monthly_closings.py
+++ max_profit_from_monthly_closings.py
@@ -7,11 +7,15 @@ from monthly_closing_prices import (
     add_max_profit_percent_from_last_closing_column, get_monthly_data
 )
 
-def main(MONTHLY_PROFIT_LAALACH=4, NO_OF_MONTHS=18, verbose=False):
+def main(MONTHLY_PROFIT_LAALACH=4, NO_OF_MONTHS=18, verbose=False, output_format='human'):
     '''
         Finds the probability that a stock bought at a month's closing returned
         MONTHLY_PROFIT_LAALACH percent of profit in the next month, in the
         last `NO_OF_MONTHS` months
+        Args:
+            format:
+                If 'tsv' -> results are printed in tab-separated format
+                elif 'human' -> results are printed in human readable format
     '''
     today = datetime.datetime.today()
     for symbol in NIFTY50:
@@ -28,7 +32,11 @@ def main(MONTHLY_PROFIT_LAALACH=4, NO_OF_MONTHS=18, verbose=False):
                 PROFITABLE_MONTHS_COUNT += 1
         if verbose:
             print(monthly_data)
-        print('CONCLUSION ------ ', symbol, PROFITABLE_MONTHS_COUNT / NO_OF_MONTHS * 100)
+        output_string = ''
+        if output_format == 'human':
+            output_string += 'CONCLUSION ------ '
+        output_string += '%s\t%s' % (symbol, str(PROFITABLE_MONTHS_COUNT / NO_OF_MONTHS * 100))
+        print(output_string)
 
 
 if __name__ == '__main__':
